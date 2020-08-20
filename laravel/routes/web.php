0fa860e090b4d8fe615bac0bb660799b1a2469a6
anton2009danilov <anton2009danilov@yandex.ru>
2020-08-21 02:58:56 +0300

    Для тестирования работы сделал простые Route для каждой страницы

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -14,7 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
-$a = 'test commit';
+Route::get('/calendar', function () {
+    return view('calendar');
+});
+
+Route::get('/alreadyDoneExercises', function () {
+    return view('alreadyDoneExercises');
+});
+
+Route::get('/exercise', function () {
+    return view('exercise');
+});
