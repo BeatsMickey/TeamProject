a00c0946745586da620df83093be00dc0fa07e09
anton2009danilov <anton2009danilov@yandex.ru>
2020-08-24 16:20:34 +0300

    Поправки для автоматизации заполнения базы данных по команде: php artisan migrate:refresh --seed

--- laravel/database/seeds/DatabaseSeeder.php
+++ laravel/database/seeds/DatabaseSeeder.php
@@ -12,5 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        $this->call(ExerciseCategoriesSeeder::class);
+        $this->call(ExercisesSeeder::class);
+        $this->call(ExerciseCategoriesRelationsSeeder::class);
     }
 }
