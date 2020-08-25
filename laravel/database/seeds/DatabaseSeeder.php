2dd92822f025d40c080acb99d90ade8487e926b8
anton2009danilov <anton2009danilov@yandex.ru>
2020-08-25 23:05:12 +0300

    Миграция создания таблицы наборов упражнений, seeder для её заполнения

--- laravel/database/seeds/DatabaseSeeder.php
+++ laravel/database/seeds/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
         $this->call(ExercisesSeeder::class);
         $this->call(ExerciseCategoriesRelationsSeeder::class);
         $this->call(UsersSeeder::class);
+        $this->call(SetsSeeder::class);
     }
 }
