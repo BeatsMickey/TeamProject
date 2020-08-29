b435103979bef403912840da9ad51e3ec13deb11
anton2009danilov <anton2009danilov@yandex.ru>
2020-08-29 19:21:51 +0300

    подклюдчил основной шаблон к страницам программы

--- laravel/resources/views/program/one.blade.php
+++ laravel/resources/views/program/one.blade.php
@@ -1,10 +1,23 @@
-<h3>{{ $program->name }}</h3>
-@foreach($sets as $key => $set)
-    <h2>День {{ $set->pivot->day_of_program }} - {{ $set->name }}</h2>
-    @foreach($set->exercises as $exercise)
-        <p>{{ $exercise->name }}: вес {{ $exercise->pivot->weight }} кг, {{ $exercise->pivot->repetitions }} повторений</p>
+@extends('layouts.app')
+@section('title', 'Страница программы')
+
+@section('header')
+    @parent
+@endsection
+
+@section('content')
+    <h3>{{ $program->name }}</h3>
+    @foreach($sets as $key => $set)
+        <h2>День {{ $set->pivot->day_of_program }} - {{ $set->name }}</h2>
+        @foreach($set->exercises as $exercise)
+            <p>{{ $exercise->name }}: вес {{ $exercise->pivot->weight }} кг, {{ $exercise->pivot->repetitions }} повторений</p>
+        @endforeach
     @endforeach
-@endforeach
-<a href="{{ route('program.choose', $program->id) }}">Выбрать эту программу</a>
+    <a href="{{ route('program.choose', $program->id) }}">Выбрать эту программу</a>
+@endsection
+
+
+
+
 
 
