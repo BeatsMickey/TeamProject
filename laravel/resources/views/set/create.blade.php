4c6022e0124ccbff35e77099e6d40ab77172ddf4
anton2009danilov <anton2009danilov@yandex.ru>
2020-09-08 19:03:18 +0300

    Поместил контент блока "Старница наборов упражнений" в <div class="section__container">

--- laravel/resources/views/set/create.blade.php
+++ laravel/resources/views/set/create.blade.php
@@ -6,39 +6,39 @@
 @endsection
 
 @section('content')
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-
-    <h3>Создать новый набор упражнений</h3>
-    <form action="{{ route('set.create') }}" method="post">
-        @csrf
-        <div>
-            <input type="text" name="name" placeholder="Название набора">
-        </div>
-
-        @for ($i = 1; $i <= 4; $i++)
-            <div>
-                <select name="exercise_{{ $i }}" id="day_one">
-                    <option value=""></option>
-                    @foreach($exercises as $exercise)
-                        <option value="{{ $exercise->id }}">{{ $exercise->name }}</option>
+    <div class="section__container">
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
                     @endforeach
-                </select>
+                </ul>
             </div>
-        @endfor
+        @endif
+
+        <h3>Создать новый набор упражнений</h3>
+        <form action="{{ route('set.create') }}" method="post">
+            @csrf
+            <div>
+                <input type="text" name="name" placeholder="Название набора">
+            </div>
+
+            @for ($i = 1; $i <= 4; $i++)
+                <div>
+                    <select name="exercise_{{ $i }}" id="day_one">
+                        <option value=""></option>
+                        @foreach($exercises as $exercise)
+                            <option value="{{ $exercise->id }}">{{ $exercise->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+            @endfor
 
 
-
-        <button>Создать набор упражнений</button>
-    </form>
-
+            <button>Создать набор упражнений</button>
+        </form>
+    </div>
 @endsection
 
 
--- laravel/resources/views/set/index.blade.php
+++ laravel/resources/views/set/index.blade.php
@@ -6,28 +6,30 @@
 @endsection
 
 @section('content')
-    @if(session('message'))
-        <h3>{{ session('message') }}</h3>
-    @endif
-    <h2>Старница наборов упражнений</h2>
-    <div>
-        <h3>Наборы упражнений</h3>
-        @foreach($sets as $set)
-            <div>
-                <h3><a href="{{ route('set.update', $set['id']) }}">{{ $set['name'] }}</a>
-                    <a href="{{ route('set.destroy', $set) }}"> (удалить)</a>
-                </h3>
-                <ul>
-                    @foreach($set->exercises as $exercise)
-                        <li>{{ $exercise->name }}</li>
-                    @endforeach
-                </ul>
-            </div>
-            <p>
-            </p>
-        @endforeach
+    <div class="section__container">
+        @if(session('message'))
+            <h3>{{ session('message') }}</h3>
+        @endif
+        <h2>Старница наборов упражнений</h2>
+        <div>
+            <h3>Наборы упражнений</h3>
+            @foreach($sets as $set)
+                <div>
+                    <h3><a href="{{ route('set.update', $set['id']) }}">{{ $set['name'] }}</a>
+                        <a href="{{ route('set.destroy', $set) }}"> (удалить)</a>
+                    </h3>
+                    <ul>
+                        @foreach($set->exercises as $exercise)
+                            <li>{{ $exercise->name }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+                <p>
+                </p>
+            @endforeach
+        </div>
+        <div class="hr"></div>
+        <div><a href="{{ route('set.create') }}">Создать новый набор упражнений</a></div>
+        <div><a href="{{ route('program.index') }}">Перейти на страницу программ упражнений</a></div>
     </div>
-    <div class="hr"></div>
-    <div><a href="{{ route('set.create') }}">Создать новый набор упражнений</a></div>
-    <div><a href="{{ route('program.index') }}">Перейти на страницу программ упражнений</a></div>
 @endsection
