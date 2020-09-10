d5acab137f9a708855e192e8d84d7e17bf9d7a05
anton2009danilov <anton2009danilov@yandex.ru>
2020-09-10 23:45:49 +0300

    стили для страници "мои упражнения" - программы

--- laravel/resources/views/program/index.blade.php
+++ laravel/resources/views/program/index.blade.php
@@ -10,30 +10,40 @@
         @if(session('message'))
             <h3 class="my-program_message">{{ session('message') }}</h3>
         @endif
-        <h2 class="">Страница программы занятий</h2>
+{{--        <h2 class="my-program_header">Страница программы занятий</h2>--}}
+{{--        <div class="hr"></div>--}}
+{{--        @if($current_program)--}}
+{{--            <div class="my-program_section">--}}
+{{--            --}}{{--                <h3 class="my-program_header">Активная программа</h3>--}}
+{{--                <a href="{{ route('program.show', $current_program['id']) }}">{{ $current_program['name'] }}</a><br>--}}
+{{--                <a href="{{ route('program.reset', $current_program->id) }}">Сбросить программу</a>--}}
+{{--            </div>--}}
 
-        @if($current_program)
-            <div>
-                <h3>Активная программа</h3>
-                <a href="{{ route('program.show', $current_program['id']) }}">{{ $current_program['name'] }}</a><br>
-                <a href="{{ route('program.reset', $current_program->id) }}">Сбросить программу</a>
-            </div>
-            <div class="hr"></div>
-        @endif
+{{--        @endif--}}
 
         <div>
-            <h3>Перечень программ</h3>
-            @foreach($programs as $program)
-                <p>
-                    <a href="{{ route('program.show', $program['id']) }}">{{ $program['name'] }}</a>
-                    <a href="{{ route('program.destroy', $program) }}"> [X]</a>
-                </p>
-            @endforeach
+            <h3>Выберете программу занятий:</h3>
+            <ul>
+                @foreach($programs as $program)
+                    <li class="my-program_list-item">
+                        @if($current_program && $current_program->id === $program->id)
+                            <a class="my-program_list-link" href="{{ route('program.show', $program['id']) }}">{{ $program['name'] }}
+                                <span class="my-program_selected-item" >(Активная программа)</span>
+                            </a>
+                            <a class="my-program_list-link" href="{{ route('program.reset', $current_program->id) }}">Сбросить программу</a>
+                        @else
+                            <a class="my-program_list-link" href="{{ route('program.show', $program['id']) }}">{{ $program['name'] }}</a>
+                            <a class="my-program_list-link" href="{{ route('program.choose', $program->id) }}">(Выбрать эту программу)</a>
+                            <a class="my-program_list-link" href="{{ route('program.destroy', $program) }}"> [X]</a>
+                        @endif
+                    </li>
+                @endforeach
+            </ul>
         </div>
+        <a class="my-program_btn" href="{{ route('program.create') }}">Создать новую программу</a>
         <div class="hr"></div>
-        <div><a href="{{ route('program.create') }}">Создать новую программу</a></div>
-        <div><a href="{{ route('set.index') }}">Перейти на страницу наборов упражнений</a></div>
-        <div><a href="{{ route('exercises.all') }}">Перейти на страницу упражнений</a></div>
+        <div><a class="" href="{{ route('set.index') }}">Перейти на страницу наборов упражнений</a></div>
+        <div><a class="" href="{{ route('exercises.all') }}">Перейти на страницу упражнений</a></div>
     </div>
 
 @endsection
